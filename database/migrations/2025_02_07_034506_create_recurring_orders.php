f92b868b13829d187cc41b7fec53b2de7a0eee05
prit <prit@indianinfotech.org>
2025-02-07 09:33:39 +0530

    add recurring order and details table

---
+++ database/migrations/2025_02_07_034506_create_recurring_orders.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recurring_orders', function (Blueprint $table) {
+            $table->id();
+            $table->enum('order_period', ['daily', 'weekly', 'monthly'])->nullable();
+            $table->dateTime('last_created_date')->nullable();
+            $table->enum('payment_cycle', ['daily', 'weekly', 'monthly'])->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recurring_orders');
+    }
+};
