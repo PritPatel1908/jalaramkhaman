f1e8296c024a4f5b121abb955d8005ce3563a709
PritPatel1908 <kachhadiyaprit89039@gmail.com>
2025-03-10 06:55:59 +0530

    add condition to not show super user login in another user

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -186,4 +186,11 @@ class UserResource extends Resource
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
+
+    public static function getEloquentQuery(): Builder
+    {
+        $query = static::getModel()::query();
+        $query->where("user_id", '!=', 1);
+        return $query;
+    }
 }
